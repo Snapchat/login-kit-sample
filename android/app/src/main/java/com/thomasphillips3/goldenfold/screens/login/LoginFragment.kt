bf1bb759797a7ec465f50cffa819f614e3bf1ed6
Thomas Phillips <thomasphillips3@gmail.com>
2021-03-28 11:12:34 -0700

    implement login state changed listener

--- android/app/src/main/java/com/thomasphillips3/goldenfold/screens/login/LoginFragment.kt
+++ android/app/src/main/java/com/thomasphillips3/goldenfold/screens/login/LoginFragment.kt
@@ -1,27 +1,53 @@
 package com.thomasphillips3.goldenfold.screens.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.snapchat.kit.sdk.SnapLogin
+import com.snapchat.kit.sdk.core.controller.LoginStateController
 import com.thomasphillips3.goldenfold.R
 import com.thomasphillips3.goldenfold.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    private var mLoginStateChangedListener: LoginStateController.OnLoginStateChangedListener? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding: FragmentLoginBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_login,
+                container,
+                false)
 
         binding.loginButton.setOnClickListener {
-            SnapLogin.getAuthTokenManager(context).startTokenGrant()
+            SnapLogin.getAuthTokenManager(context)
+                .startTokenGrant()
+
+            SnapLogin.getLoginStateController(context)
+                .addOnLoginStateChangedListener(mLoginStateChangedListener)
+        }
+
+        mLoginStateChangedListener = object :
+            LoginStateController.OnLoginStateChangedListener {
+            override fun onLoginSucceeded() {
+                // Here you could update UI to show login success
+            }
+
+            override fun onLoginFailed() {
+                // Here you could update UI to show login failure
+            }
+
+            override fun onLogout() {
+                // Here you could update UI to reflect logged out state
+            }
         }
         return binding.root
     }
